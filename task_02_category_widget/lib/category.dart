08c3fc38037c67f025127c6952c71d498c54d4fb
Talha Salman <talha.salman22@gmail.com>
2021-06-10 13:03:03 +0500

    Cleaned .dart files
    Removed unnecessary comments from the .dart files that translated over form the udacity course repo.

--- task_02_category_widget/lib/category.dart
+++ task_02_category_widget/lib/category.dart
@@ -1,9 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// To keep your imports tidy, follow the ordering guidelines at
-// https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
 /// A custom [Category] widget.
@@ -18,11 +12,6 @@ class Category extends StatelessWidget {
   final IconData icon;
   final MaterialColor color;
 
-  /// Creates a [Category].
-  ///
-  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
-  /// the UI, and the icon that represents it (e.g. a ruler).
-  // DONE: You'll need the name, color, and iconLocation from main.dart
   const Category({
     Key key,
     @required this.name,
@@ -30,16 +19,8 @@ class Category extends StatelessWidget {
     @required this.color,
   }) : super(key: key);
 
-  /// Builds a custom widget that shows [Category] information.
-  ///
-  /// This information includes the icon, name, and color for the [Category].
   @override
-  // This `context` parameter describes the location of this widget in the
-  // widget tree. It can be used for obtaining Theme data from the nearest
-  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
     return Material(
       color: Colors.transparent,
       child: Container(
--- task_02_category_widget/lib/main.dart
+++ task_02_category_widget/lib/main.dart
@@ -1,27 +1,14 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
-
-// You can use a relative import, i.e. `import 'category.dart';` or
-// a package import, as shown below.
-// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
 import 'package:task_02_category_widget/category.dart';
 
-// Done: Pass this information into your custom [Category] widget
 const _categoryName = 'Cake';
 const _categoryIcon = Icons.cake;
 const _categoryColor = Colors.green;
 
-/// The function that is called when main.dart is run.
 void main() {
   runApp(UnitConverterApp());
 }
 
-/// This widget is the root of our application.
-/// Currently, we just show one widget in our app.
 class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -31,7 +18,6 @@ class UnitConverterApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.green[100],
         body: Center(
-          // DONE: Determine what properties you'll need to pass into the widget
           child: Category(
             name: _categoryName,
             icon: _categoryIcon,
